8d2611751e894af213b5f5fa925a1adaf5e3fe30
Fi1osof <n.lanets@modxclub.ru>
2017-10-02 03:26:55 +0300

    counters

--- assets/components/modxsite/templates/v9/server/routes/main/index.js
+++ assets/components/modxsite/templates/v9/server/routes/main/index.js
@@ -363,14 +363,24 @@ module.exports = function (options) {
       <!DOCTYPE html>
         <html>
         <head>
-            <meta charset="utf-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Городские бани</title>
-            <link rel="stylesheet" href="${assetsUrl}css/main.css">
-            <base href="/" />
-            <script type="application/javascript">
-              window.REDUX_INITIAL_STATE = ${JSON.stringify(initialState)};
-            </script>
+          <meta charset="utf-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>Городские бани</title>
+          <link rel="stylesheet" href="${assetsUrl}css/main.css">
+          <base href="/" />
+          <script type="application/javascript">
+            window.REDUX_INITIAL_STATE = ${JSON.stringify(initialState)};
+          </script>
+
+          <script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+            ga('create', 'UA-39491207-3', 'auto');
+            ga('send', 'pageview');
+          </script>
+        
         </head>
         <body>
           <div id="root">${componentHTML}</div>
